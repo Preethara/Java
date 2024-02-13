a40b506afcd924025b7b054add79d3735d453103
Preetha Ramesh <51361920+Preethara@users.noreply.github.com>
2024-02-13 10:00:31 +0000

    Update Threads.java

--- Threads.java
+++ Threads.java
@@ -1,10 +1,22 @@
 package com.codetodevop.javaoopsconcept;
-
 public class Threads {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+			A object1 = new A();
+			B object2 = new B();
+			object1.start();
+			object2.start();
 	}
-
+}
+class A extends thread {
+	public void run()
+	{
+		System.out.println("In Class A show method");
+	}
+}
+class B extends thread{
+	public void run()
+	{
+		System.out.println("In Class B show method");
+	}
 }
